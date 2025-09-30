8856abe2e2170008d9d9918dcd93db53c789017a
sunilskut <sunilskut@gmail.com>
2025-09-30 08:56:17 +0530

    Delete fbc.js

--- fbc.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBsTc2h-cTOCXsKjFP3tn83-Dokseci7rk",
-  authDomain: "ksrtckmr-2d890.firebaseapp.com",
-  projectId: "ksrtckmr-2d890",
-  storageBucket: "ksrtckmr-2d890.firebasestorage.app",
-  messagingSenderId: "775551270886",
-  appId: "1:775551270886:web:4b0a3d9b069f34e0582027",
-  measurementId: "G-JMYNYS10VS"
-};
