90d54b92413ea81c5a1dd5f94113581ae89e2f33
sunilskut <sunilskut@gmail.com>
2025-10-01 09:39:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import Page1 from "./Page1";
