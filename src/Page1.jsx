0320678db83e4294e24d9052c8d42c15c89a6c41
sunilskut <sunilskut@gmail.com>
2025-10-01 08:15:57 +0530

    Save local changes before pull

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
 export default function Page1() {
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-100">
+     <div className="h-screen w-screen flex flex-col items-center justify-center bg-green-100 space-y-6">
       <h1 className="text-4xl font-bold text-blue-700">This is Page 1</h1>
+ <Link
+          to="/dashboard"
+          className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+        >
+          Go Back
+        </Link>
     </div>
   );
 }
